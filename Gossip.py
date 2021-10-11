6af171ee84b55e2392756e880f3d98355ce1bfbd
Daniel Wzoreck <wzoreck2000@gmail.com>
2021-10-11 13:29:18 -0300

    Loading configsfile

--- Gossip.py
+++ Gossip.py
@@ -1,5 +1,7 @@
 import socket 
-import threading 
+import threading
+import configparser
+import sys
    
 def workerThread(s):
     while True: 
@@ -14,14 +16,30 @@ def workerThread(s):
     s.close() 
   
 def Main(): 
-    host = "" 
-    port = 2802
+
+    # Obtendo configuracao passada por parametro
+    config = configparser.RawConfigParser()
+    config.read(sys.argv[1])
+
+    print('\n------- Config -------')
+    print(f'Id: {config.get("config", "id")}')
+    print(f'Nó: {config.get("config", "node")}')
+    print(f'Porta: {config.get("config", "port")}')
+    print(f'IP: {config.get("config", "ip")}')
+    print(f'Vizinho 1 porta: {config.get("config", "neighbor1_port")}')
+    print(f'Vizinho 2 porta: {config.get("config", "neighbor2_port")}')
+
+
+    #
+
+    host = str(config.get("config", "ip"))
+    port = int(config.get("config", "port"))
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
     server_socket.bind((host, port)) 
     server_socket.listen() 
   
-    print("Servidor inicializado na porta " + str(port))
+    print("\nServidor inicializado na porta " + str(port))
 
     while True: 
         s, addr = server_socket.accept() 
