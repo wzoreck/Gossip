f5725ed2dfabb6bf12bb669d05e6ef31cce72c51
Gustavo-F <fgustavo016@gmail.com>
2021-10-15 14:00:29 -0300

    Added message replication

--- Gossip.py
+++ Gossip.py
@@ -6,10 +6,11 @@ import sys
 
 received_messages = []
 
-def workerThread(s):
+def workerThread(s, neighbors_ports):
     while True:
         print(f'\nMensagens atuais: {received_messages}')
 
+        print('Aguardando Mensagem...')
         msg = s.recv(1024)
         if not msg: break
 
@@ -24,18 +25,22 @@ def workerThread(s):
             if has_message:    
                 response = 'Já tenho essa mensagem.'
             else:
-                received_messages.append(decoded_message)
                 response = 'Não tenho essa mensagem.'
+                received_messages.append(decoded_message)
 
             print(response)
             s.send(response.encode('UTF-8'))  
+
+            if not has_message:
+                replicate_thread = threading.Thread(target=clientThread, args=[neighbors_ports, decoded_message, True])
+                replicate_thread.start()
         except:
             print('Erro ao responder.')
             
     s.close()
 
 
-def receiverThread(server_socket):
+def receiverThread(server_socket, neighbors_ports):
     print('\nThread de recebimento iniciada...')
     
     while True: 
@@ -44,16 +49,17 @@ def receiverThread(server_socket):
         s, addr = server_socket.accept() 
         print('\nCliente Conectado:', addr[0], ':', addr[1])
 
-        tw = threading.Thread(target=workerThread, args=[s])
+        tw = threading.Thread(target=workerThread, args=[s, neighbors_ports])
         tw.start()
 
   
-def client_thread(neighbors_ports, socket_port=None):
+def clientThread(neighbors_ports, msg=None, break_at_finish=False):
     print('Thread de envio iniciada...')
 
     while True:
-        msg = input('Envie uma mensagem: ')
-        received_messages.append(msg)
+        if not msg:
+            msg = input('Envie uma mensagem: ')
+            received_messages.append(msg)
 
         for port in neighbors_ports:
             dest = ('127.0.0.1', int(port))
@@ -71,15 +77,15 @@ def client_thread(neighbors_ports, socket_port=None):
             
             client_socket.shutdown(socket.SHUT_RDWR)
 
+        msg = None
+        if break_at_finish: break
 
-def Main(): 
-    print('\n')
-    print('███████╗ ██████╗ ███████╗ ██████╗  ██████╗ █████╗ ████████╗ ██████╗ ██████╗ ')
-    print('██╔════╝██╔═══██╗██╔════╝██╔═══██╗██╔════╝██╔══██╗╚══██╔══╝██╔═══██╗██╔══██╗')
-    print('█████╗  ██║   ██║█████╗  ██║   ██║██║     ███████║   ██║   ██║   ██║██████╔╝')
-    print('██╔══╝  ██║   ██║██╔══╝  ██║   ██║██║     ██╔══██║   ██║   ██║   ██║██╔══██╗')
-    print('██║     ╚██████╔╝██║     ╚██████╔╝╚██████╗██║  ██║   ██║   ╚██████╔╝██║  ██║')
-    print('╚═╝      ╚═════╝ ╚═╝      ╚═════╝  ╚═════╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝')
+
+def Main():
+    print(' ___ ___  ___ ___   ___   _ _____ ___  ___ \n' 
+        + '| __/ _ \| __/ _ \ / __| /_\_   _/ _ \| _ \ \n' 
+        + '| _| (_) | _| (_) | (__ / _ \| || (_) |   / \n'
+        + '|_| \___/|_| \___/ \___/_/ \_\_| \___/|_|_\ \n')
 
     # Obtendo configuracao passada por parametro
     config = configparser.RawConfigParser()
@@ -99,11 +105,11 @@ def Main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
     server_socket.bind((host, port))
   
-    receiver_thread = threading.Thread(target=receiverThread, args=[server_socket])
+    receiver_thread = threading.Thread(target=receiverThread, args=[server_socket, neighbors_ports])
     receiver_thread.start()
 
-    client_threa = threading.Thread(target=client_thread, args=[neighbors_ports])
-    client_threa.start()
+    client_thread = threading.Thread(target=clientThread, args=[neighbors_ports])
+    client_thread.start()
 
 if __name__ == '__main__': 
     Main()
